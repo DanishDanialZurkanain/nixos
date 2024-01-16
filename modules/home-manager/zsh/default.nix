77b6e43036e1af8db05d2ffa1bf89aeff91996dd
Danish Danial <danishdanialzurkanain@gmail.com>
2024-01-16 21:05:24 +0800

    🔧  Update ZSH Alias

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -3,10 +3,12 @@
   programs.zsh = {
     enable = true;
     shellAliases = {
-      ll = "ls -l";
       ls = "eza";
       
-      update = "sudo nixos-rebuild switch --flake /home/danial/.config/nixos#marge";
+      zsh-update = "source /home/danial/.zshrc";
+
+      nix-rebuild = "sudo nixos-rebuild switch --flake /home/danial/.config/nixos#marge";
+      nix-garbage = "sudo nix-collect-garbage -d"; 
     };
   };
 }
