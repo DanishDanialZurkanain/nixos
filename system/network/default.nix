3ecaac97b8972892cd0f10787692c444f4a4c2d7
Danish Danial <danishdanialzurkanain@gmail.com>
2024-01-18 18:44:04 +0800

    ♻️ Move line to related file

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,6 +1,6 @@
+{ lib, ... }:
 {
   networking.hostName = "marge"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -8,4 +8,13 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+
 }
