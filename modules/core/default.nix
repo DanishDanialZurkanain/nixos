67f581706ff22271a8824a8f68f1c719b4e8dc51
Danish Danial <danishdanialzurkanain@gmail.com>
2024-01-18 18:44:58 +0800

    🔧  Disable XDG-Portal for now

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -3,6 +3,5 @@
           [(import ./docker-compose)]
         ++[(import ./gnome/applications.nix)]
         ++[(import ./podman)]
-        ++[(import ./xdg-portal)]
         ++[(import ./xterm)];
 }
